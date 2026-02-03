3d30fa1b903ffb16de2f66ac5bb4506496454657
PXIAOHE <105998472+PXIAOHE@users.noreply.github.com>
2026-02-03 23:15:37 +0800

    Delete HyperClick-master directory

--- HyperClick-master/app/src/androidTest/java/com/xiaohe/hyperlocklittle/ExampleInstrumentedTest.java
+++
@@ -1,7 +0,0 @@
-package com.xiaohe.hyperlocklittle;
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
